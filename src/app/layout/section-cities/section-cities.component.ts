b71d7d4a5cd8260a0e4f70d7094bcb096edc8466
muzzone <muzz0neasdqwe@gmail.com>
2019-04-27 09:27:50 +0300

    cities cards

--- src/app/layout/section-cities/section-cities.component.ts
+++ src/app/layout/section-cities/section-cities.component.ts
@@ -9,13 +9,13 @@ export class SectionCitiesComponent implements OnInit {
 
   public demoData = [
     {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
-    {name: 'Madrid', image: 'layer4.png'},
+    {name: 'Barcelona', image: 'Layer5.png'},
+    {name: 'Valencia', image: 'Layer6.png'},
+    {name: 'Sevilla', image: 'Layer7.png'},
+    {name: 'Málaga', image: 'Layer8.png'},
+    {name: 'Bilbao', image: 'Layer9.png'},
+    {name: 'Granada', image: 'Layer10.png'},
+    {name: 'Zaragoza', image: 'Layer11.png'},
   ]
 
   constructor() { }
